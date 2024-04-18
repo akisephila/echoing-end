e2e1281c16100b2deeff4780fb0e25dcefd9d16f
akisephila <77479717+akisephila@users.noreply.github.com>
2024-04-18 11:58:51 +1000

    upload generated template

---
+++ src/client/java/com/akisephila/echoing_end/EchoingEndClient.java
@@ -0,0 +1,10 @@
+package com.akisephila.echoing_end;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoingEndClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
